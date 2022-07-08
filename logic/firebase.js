4cf5ea7a638fadccf450691883219d17e1cd280b
Rafael <tecnologia@indes.org.br>
2022-07-08 16:50:51 -0300

    update

---
+++ logic/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB8XeXj9sLcF42hZ4bv7b4BZ3rYjcX2zeI",
+    authDomain: "projeto-02---to-do-list.firebaseapp.com",
+    databaseURL: "https://projeto-02---to-do-list-default-rtdb.firebaseio.com",
+    projectId: "projeto-02---to-do-list",
+    storageBucket: "projeto-02---to-do-list.appspot.com",
+    messagingSenderId: "334696651653",
+    appId: "1:334696651653:web:8f7487c345d6c5ebb682ec"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
